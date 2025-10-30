356d98c0307a889f9cd49f3dcf2dc3c50dbd049d
Shwetakankanwadi <shwetakankanwadi878@gmail.com>
2025-10-30 16:19:02 +0530

    Create student_details.py

---
+++ student_details.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+usn = input("Enter your USN: ")
+age = input("Enter your age: ")
+address = input("Enter your address: ")
+
+print("Name:", name)
+print("USN:", usn)
+print("age:", age)
+print("Address:", address)
