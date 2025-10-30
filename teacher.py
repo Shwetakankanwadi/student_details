b2e44163dbd39dc6243cbfb9b115f6ccb97e9c8a
sanjeev <sanjeevhadapad10@gmail.com>
2025-10-30 17:10:23 +0530

    save

---
+++ teacher.py
@@ -0,0 +1,8 @@
+teacher_name = input("Enter your teacher name: ")
+course_code= input("Enter your course code: ")
+subject = input("Enter your subject:")
+
+
+print("teacher_ame:", name)
+print("Course_code:", course_code)
+print("subject:", subject)
